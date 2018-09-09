fa96989fa236e555c84e92aeb368168f9175ef8d
kekhuay <nulladivides@protonmail.com>
2018-09-09 18:37:41 +0700

    add new signin route

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -7,6 +7,10 @@ function tokenForUser(user) {
     return jwt.encode({ sub: user.id, iat: timestamp }, config.secret);
 }
 
+exports.signin = function(req, res, next) {
+    res.send({ token: tokenForUser(req.user)});
+}
+
 exports.signup = function(req, res, next) {
     const email = req.body.email;
     const password = req.body.password;
@@ -35,6 +39,6 @@ exports.signup = function(req, res, next) {
             return next(err);
         }
 
-        res.json({ toen: tokenForUser(user) });
+        res.json({ token: tokenForUser(user) });
     });
 }
